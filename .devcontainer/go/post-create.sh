a2bb409f5c20f09a22286edcb4912aec98989537
Edoardo Vacchi <evacchi@users.noreply.github.com>
2025-03-31 07:52:36 +0000

    fix
    Signed-off-by: Edoardo Vacchi <evacchi@users.noreply.github.com>

--- .devcontainer/go/post-create.sh
+++ .devcontainer/go/post-create.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 wget https://github.com/tinygo-org/tinygo/releases/download/v0.37.0/tinygo_0.37.0_amd64.deb 
 sudo dpkg -i tinygo_0.37.0_amd64.deb
+rm tinygo_0.37.0_amd64.deb
 
 curl -s https://static.dylibso.com/cli/install.sh | bash
